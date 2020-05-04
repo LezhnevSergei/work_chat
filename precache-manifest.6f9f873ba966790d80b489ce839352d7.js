82d64a9db214f7ec362eb963b1f89f8a9fc772c9
GolubevSergey <golubev.sergey.petrovich@gmail.com>
2020-05-04 08:04:32 +0500

    Updates

---
+++ precache-manifest.6f9f873ba966790d80b489ce839352d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f8be61432963ed765a2a3dc4deb6b2",
+    "url": "/work_chat/index.html"
+  },
+  {
+    "revision": "ff51364f0c5acc25e137",
+    "url": "/work_chat/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8311f34c551c6e74b8db",
+    "url": "/work_chat/static/css/main.b386b193.chunk.css"
+  },
+  {
+    "revision": "ff51364f0c5acc25e137",
+    "url": "/work_chat/static/js/2.d3f775be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/work_chat/static/js/2.d3f775be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8311f34c551c6e74b8db",
+    "url": "/work_chat/static/js/main.5bd41fe0.chunk.js"
+  },
+  {
+    "revision": "fbc6f1a8005460e581f4",
+    "url": "/work_chat/static/js/runtime-main.2fbcb370.js"
+  }
+]);
